3d51c2b10e29839d1438fdf88dcae81434b53eec
DomCastaneda <dcastaneda2828@gmail.com>
2021-11-01 16:13:51 -0700

    Removed PartPriceTest
    Removed because it is outdated

--- test/webscrape/PartPriceTest.java
+++ test/webscrape/PartPriceTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
  *
  * @author dcast
  */
+/*
 public class Part {
     
     public Part() {
@@ -29,7 +30,7 @@ public class Part {
 
     /**
      * Test of getCost method, of class Part.
-     */
+    //
     @Test
     public void testGetCost() {
         System.out.println("getCost");
@@ -42,3 +43,4 @@ public class Part {
     }
     
 }
+*/
