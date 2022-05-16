9f956d4b00c24abc328b53b2fa45a8daa64d5318
Jaewon Park <jaepark@chapman.edu>
2022-05-16 03:26:32 -0700

    Added Profile Page

---
+++ src/turboparts/ProfilePageController.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package turboparts;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author jaepark
+ */
+public class ProfilePageController implements Initializable 
+{
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
+    public void switchToSearch(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("SearchPage.fxml"));
+        
+        /* scene switch */
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    public void switchToProfile(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("ProfilePage.fxml"));
+        
+        /* scene switch */
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    public void switchToSetting(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("SettingsPage.fxml"));
+        
+        /* scene switch */
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) 
+    {
+        // TODO
+    }    
+    
+}
