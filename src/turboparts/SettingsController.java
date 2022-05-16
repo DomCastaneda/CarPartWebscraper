41fcca60f1a16cddbfe24c7e21c4ced0166f9ef7
Jaewon Park <jaepark@chapman.edu>
2022-05-15 22:27:35 -0700

    Update SettingsController.java

--- src/turboparts/SettingsController.java
+++ src/turboparts/SettingsController.java
@@ -9,32 +9,62 @@ package turboparts;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class SettingsController implements Initializable {
+/**
+ * Settings Controller class
+ *
+ * @author jaepark
+ */
+
+
+public class SettingsController implements Initializable 
+{
     
     private Stage stage;
     private Scene scene;
     private Parent root;
     
+    @FXML
+    private Button confirm;
+    
+    @FXML 
+    private TextField username;
+    
+    @FXML
+    private PasswordField password;
+    
+    @FXML 
+    private ComboBox comboBox;
+    
+    @FXML 
+    private Label label;
+    
+    @FXML 
+    private Button button;
+    
+    @FXML 
+    private AnchorPane mainPane;
+    
+    
     public void switchToSearch(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("SearchPage.fxml"));
-        
-        /* scene switch */
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    
-    public void switchToSaved(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("SavedPage.fxml"));
         
         /* scene switch */
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -53,11 +83,79 @@ public class SettingsController implements Initializable {
         stage.show();
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void switchToProfile(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("ProfilePage.fxml"));
         
+        /* scene switch */
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) 
+    {
+        confirm.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (!username.getText().trim().isEmpty() && !password.getText().trim().isEmpty()) {
+                    DBUtils.updateUser(event, username.getText(), password.getText());
+                }  else {
+                    System.out.println("Please fill in all information.");
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setContentText("Please fill in username and password.");
+                    alert.show();
+                }
+            }
+        });
+        
+        ToggleSwitch button = new ToggleSwitch();
+        SimpleBooleanProperty isOn = button.switchOnProperty();
+        isOn.addListener((observable, oldValue, newValue) -> {
+            
+            if(newValue)
+            {
+                button.getScene().getRoot().getStylesheets().add(getClass().getResource("style.css").toString());
+                System.out.println("Removing the css");                
+            }
+            else
+            {
+                button.getScene().getRoot().getStylesheets().remove(getClass().getResource("style.css").toString());     
+                System.out.println("Removing the css");
+            }
+    });
+        mainPane.getChildren().add(button);
     }
 }
+
+//class ComboBoxExperiments extends Application  
+//{
+//
+//
+//    @Override
+//    public void start(Stage primaryStage) throws Exception {
+//        primaryStage.setTitle("ComboBox Experiment 1");
+//
+//        ComboBox comboBox = new ComboBox();
+//
+//        comboBox.getItems().add("Choice 1");
+//        comboBox.getItems().add("Choice 2");
+//        comboBox.getItems().add("Choice 3");
+//
+//
+//        HBox hbox = new HBox(comboBox);
+//
+//        Scene scene = new Scene(hbox, 200, 120);
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        Application.launch(args);
+//    }
+//}
 
 class changeUser{
     public String username;
